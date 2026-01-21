6980dc272bade577d12aa3c31f7cb38a35d25944
kevinn <kevin@gmail.com>
2026-01-21 17:23:57 -0300

    agregando BD PASO-1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+
+  firebaseConfig: {
+    apiKey: "AIzaSyBZGrCAg_-cRVd0cENrVGUiKC5SIBc_kkQ",
+    authDomain: "produc-admin-ad11a.firebaseapp.com",
+    projectId: "produc-admin-ad11a",
+    storageBucket: "produc-admin-ad11a.firebasestorage.app",
+    messagingSenderId: "856950392222",
+    appId: "1:856950392222:web:ad569e8996b7b9fcdfc28f",
+    measurementId: "G-QRWC1B0B9C"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  firebaseConfig: {
+    apiKey: "AIzaSyBZGrCAg_-cRVd0cENrVGUiKC5SIBc_kkQ",
+    authDomain: "produc-admin-ad11a.firebaseapp.com",
+    projectId: "produc-admin-ad11a",
+    storageBucket: "produc-admin-ad11a.firebasestorage.app",
+    messagingSenderId: "856950392222",
+    appId: "1:856950392222:web:ad569e8996b7b9fcdfc28f",
+    measurementId: "G-QRWC1B0B9C"
+  }
 };
 
 /*
