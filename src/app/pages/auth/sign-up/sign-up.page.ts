7ba9299545e5f5e62b12e23cca889302f017a7e3
kevinn <kevin@gmail.com>
2026-01-29 14:21:28 -0300

    registro de personas

--- src/app/pages/auth/sign-up/sign-up.page.ts
+++ src/app/pages/auth/sign-up/sign-up.page.ts
@@ -1,20 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { FormControl, FormGroup, FormsModule, Validators, ReactiveFormsModule } from '@angular/forms';
+import { HeaderComponent } from 'src/app/shared/components/header/header.component';
+import { CustomInputComponent } from 'src/app/shared/components/custom-input/custom-input.component';
+import { LogoComponent } from 'src/app/shared/components/logo/logo.component';
+import { FirebaseServices } from 'src/app/services/firebase.services';
+import { User } from 'src/app/models/user.model';
+import { UtilsServices } from 'src/app/services/utils.services';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule],
+  imports: [IonicModule, CommonModule, FormsModule, ReactiveFormsModule, HeaderComponent, CustomInputComponent, LogoComponent],
   templateUrl: './sign-up.page.html',
   styleUrls: ['./sign-up.page.scss'],
 })
 export class SignUpPage implements OnInit {
 
-  constructor() { }
+  form = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+    name: new FormControl('', [Validators.required, Validators.minLength(4)])
+  })
+
+  firebaseSvc = inject(FirebaseServices);
+  utilsSvc = inject(UtilsServices);
+
 
   ngOnInit() {
   }
 
+  async submit() {
+    if (this.form.valid) {
+      const loading = await this.utilsSvc.loading();
+      await loading.present();
+
+      this.firebaseSvc.signUp(this.form.value as User).then(async res => {
+
+        await this.firebaseSvc.updateUser(this.form.value.name);
+
+        console.log(res);
+
+      }).catch(error => {
+        console.log(error);
+
+        this.utilsSvc.presentToast({
+          message: error.message,
+          duration: 2500,
+          color: 'danger',
+          position: 'middle',
+          icon: 'alert-circle-outline'
+        });
+
+      }).finally(() => {
+        loading.dismiss();
+      });
+    }
+  }
 }
--- src/app/services/firebase.services.ts
+++ src/app/services/firebase.services.ts
@@ -1,6 +1,7 @@
 import { Injectable, inject } from '@angular/core';
-import { Auth, signInWithEmailAndPassword } from '@angular/fire/auth';
+import { Auth, signInWithEmailAndPassword, createUserWithEmailAndPassword, updateProfile } from '@angular/fire/auth';
 import { User } from '../models/user.model';
+import { getAuth } from 'firebase/auth';
 
 @Injectable({
   providedIn: 'root',
@@ -11,10 +12,26 @@ export class FirebaseServices {
 
   // ======================== authentication ========================
 
-
-
   // ========================    acceder     ======================== 
   signIn(user: User) {
     return signInWithEmailAndPassword(this.auth, user.email, user.password);
+  }
+
+  // ========================    registrar     ========================
+  signUp(user: User) {
+    return createUserWithEmailAndPassword(this.auth, user.email, user.password);
+  }
+
+  // ======================== Actualizar Usuario ========================
+  updateUser(displayName: string) {
+    const auth = getAuth();
+    // Validación CLAVE: Si no hay usuario, no hacemos la petición
+    if (auth.currentUser) {
+      return updateProfile(auth.currentUser, { displayName: displayName });
+    } else {
+      // Retornamos una promesa resuelta sin hacer nada, evitando el error de Bad Request
+      console.warn('No hay usuario logueado para actualizar perfil');
+      return Promise.resolve();
+    }
   }
 }
