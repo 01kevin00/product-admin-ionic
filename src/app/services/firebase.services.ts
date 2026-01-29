6b81a7d639df77dd180d496e14b8c199c16cbe9f
kevinn <kevin@gmail.com>
2026-01-29 14:43:26 -0300

    corregir error de inicialización de Firebase y contexto de inyección en Auth

--- src/app/services/firebase.services.ts
+++ src/app/services/firebase.services.ts
@@ -1,7 +1,33 @@
-import { Injectable, inject } from '@angular/core';
-import { Auth, signInWithEmailAndPassword, createUserWithEmailAndPassword, updateProfile } from '@angular/fire/auth';
+import { Injectable, NgModule, inject } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
+import { AppComponent } from '../app.component';
+import { AppRoutingModule } from '../app-routing.module';
+
+// # Agregados para la inicialización de Firebase
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { Auth, getAuth, provideAuth } from '@angular/fire/auth';
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
+import { environment } from '../../environments/environment';
 import { User } from '../models/user.model';
-import { getAuth } from 'firebase/auth';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+  ],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideAuth(() => getAuth())
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule { }
 
 @Injectable({
   providedIn: 'root',
@@ -24,10 +50,11 @@ export class FirebaseServices {
 
   // ======================== Actualizar Usuario ========================
   updateUser(displayName: string) {
-    const auth = getAuth();
+    const currentUser = this.auth.currentUser;
+
     // Validación CLAVE: Si no hay usuario, no hacemos la petición
-    if (auth.currentUser) {
-      return updateProfile(auth.currentUser, { displayName: displayName });
+    if (currentUser) {
+      return updateProfile(currentUser, { displayName: displayName });
     } else {
       // Retornamos una promesa resuelta sin hacer nada, evitando el error de Bad Request
       console.warn('No hay usuario logueado para actualizar perfil');
